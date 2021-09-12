0e208a57e13b3c18b932f74a3684a51413f4d3a1
Orlan Quijada <orlantquijada@gmail.com>
2021-09-12 21:45:22 +0800

    testing support

---
+++ pages/index.spec.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable no-undef */
+import '@testing-library/jest-dom'
+
+import { render, screen } from '@testing-library/react'
+
+import Home from './index'
+
+describe('Lading Page', () => {
+  it('renders heading', () => {
+    render(<Home />)
+
+    const heading = screen.getByRole('heading', {
+      name: 'Hello World',
+    })
+
+    expect(heading).toBeInTheDocument()
+  })
+})
