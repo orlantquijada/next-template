23a73559ea0b72aeadf7b3ba219bbafeb099c71d
Orlan Quijada <orlantquijada@gmail.com>
2021-09-19 16:02:59 +0800

    fix: tests on /pages causing build error

--- tests/pages/index.spec.tsx
+++ tests/pages/index.spec.tsx
@@ -3,7 +3,7 @@ import '@testing-library/jest-dom'
 
 import { render, screen } from '@testing-library/react'
 
-import Home from './index'
+import Home from '@/pages/index'
 
 describe('Lading Page', () => {
   it('renders heading', () => {
